4047e4693c043f9605cc28cc0ab9e29d72a0ad1d
Adhita Prasetyo <ditzdee@gmail.com>
2020-12-10 12:20:02 +0700

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  console.log('users' + req.query.get);
+  res.send('respond with a resource');
+});
+
+// app.get('/', (req, res) => {
+//   console.log(req.query.get)
+//   res.send('Hello World!')
+// })
+
+module.exports = router;
